ef00b1015aa78d0223b242380e744030f29b03f4
Forheen Ahmed <forheen2017@gmail.com>
2023-03-17 03:55:53 +0530

    dashboard ui

---
+++ app/src/main/java/com/example/vscore/Organizer/ui/RoomCreate.kt
@@ -0,0 +1,14 @@
+package com.example.vscore.Organizer.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.vscore.R
+
+class RoomCreate : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_org_roomcreate)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/vscore/Room/ui/Dashboard.kt
@@ -0,0 +1,14 @@
+package com.example.vscore.Room.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.vscore.R
+
+class Dashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_room_dashboard)
+    }
+
+
+}
