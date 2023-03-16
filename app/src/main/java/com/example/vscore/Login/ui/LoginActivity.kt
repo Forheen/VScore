aba57e239410b3f2e7f119078520aec0585a85b9
Forheen Ahmed <forheen2017@gmail.com>
2023-03-16 21:49:52 +0530

    ui changes

--- app/src/main/java/com/example/vscore/Login/ui/LoginActivity.kt
+++ app/src/main/java/com/example/vscore/Login/ui/LoginActivity.kt
@@ -1,5 +1,4 @@
 package com.example.vscore.Login.ui
-
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,8 +6,6 @@ import androidx.databinding.DataBindingUtil
 import com.example.vscore.R
 import com.example.vscore.SignUp.ui.SignUp
 import com.example.vscore.databinding.ActivityLoginBinding
-import com.example.vscore.databinding.ActivitySignUpBinding
-
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +13,6 @@ class LoginActivity : AppCompatActivity() {
         binding=DataBindingUtil.setContentView(this@LoginActivity,R.layout.activity_login)
         initListener()
     }
-
     private fun initListener() {
         binding.signUpBtn.setOnClickListener {
             val intent= Intent(this@LoginActivity,SignUp::class.java)
