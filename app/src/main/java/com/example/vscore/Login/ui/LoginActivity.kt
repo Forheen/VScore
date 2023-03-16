35fcb367b3746655755dfe8aae465de283c5d852
Pragyan <progyan.rdmi4@gmail.com>
2023-03-16 15:07:46 +0530

    retrofit base added, basic structure made for login activity, dependencies added.

---
+++ app/src/main/java/com/example/vscore/Login/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.vscore.Login.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.vscore.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
